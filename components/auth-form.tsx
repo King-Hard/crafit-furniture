8726a7253c74fe940b66afa2d6aef7da2338f6ec
King-Hard <208336213+King-Hard@users.noreply.github.com>
2026-02-27 18:37:09 +0800

    Updated Typography

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -10,15 +10,17 @@ export default function AuthForm() {
   return (
     <Card className="shadow-sm overflow-hidden">
       <CardContent className="p-6 md:p-8">
-        <form className="space-y-8">
-          <div className="text-center space-y-1">
-            <h1 className="text-4xl font-extrabold tracking-[0.2em] font-sans uppercase">
-              Crafit
-            </h1>
-            <div className="h-px w-12 bg-muted-foreground mx-auto"/>
-            <p className="text-[12px] uppercase tracking-widest text-muted-foreground font-medium">
-              Toper Furniture
-            </p>
+        <form className="space-y-6">
+          <div className="text-center">
+            <Link href="/" className="inline-block">
+              <h1 className="text-4xl font-extrabold tracking-[0.2em] font-sans uppercase py-1">
+                Crafit
+              </h1>
+                <div className="h-px w-12 bg-muted-foreground mx-auto"/>
+              <p className="text-[12px] uppercase tracking-widest text-muted-foreground font-medium py-1">
+                Toper Furniture
+              </p>
+            </Link>
           </div>
 
           <header className="space-y-1">
@@ -44,7 +46,7 @@ export default function AuthForm() {
               <div className="absolute inset-0 flex items-center">
                 <Separator />
               </div>
-              <div className="relative flex justify-center text-[10px] uppercase">
+              <div className="relative flex justify-center text-[12px] uppercase">
                 <span className="bg-card px-2 text-muted-foreground font-medium">
                   or
                 </span>
